e9de06259ee5abca54d7828cc4b656459d3633ec
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-11-19 13:58:05 +0700

    add abour us

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -42,6 +42,7 @@ const Footer = ({ alt = '', isStudy, color, bucket = "" }) => {
                             <div className="resources">
                                 <div><strong>Resources</strong></div>
                                 <div><a href="/blog">Blog</a></div>
+                                <div><a href="/blog/about-us-5634123102158848">About us</a></div>
                             </div>
                         </Grid>
                         <Grid item xs={12} sm={12} md={6}>
