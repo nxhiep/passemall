beb310564179bd7dde3bfe7201ff158e47f6c208
Hiep Nguyen <hiepnguyen@Hieps-MacBook-Pro.local>
2020-12-03 23:27:51 +0700

    setup for each app

--- src/pages/[appNameId]/[screenChild].js
+++ src/pages/[appNameId]/[screenChild].js
@@ -8,6 +8,7 @@ import { Provider, useStore } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import SEO from '../../components/SEO';
 import { APP_NEW_DOMAIN, GA_ID } from "../../config_app";
+import ErrorPage from '../../container/error';
 import Routes from '../../routes';
 import { oldUser, setScrollDownAuto } from '../../utils';
 const StudyViewScreen = dynamic(() => import('../../container/study/Study.View'), { ssr: false })
@@ -43,6 +44,36 @@ const GameChildScreen = ({ appInfoState, url }) => {
         </>
     )
 }
+
+
+function ScreenChild({ appInfoState }) {
+    const router = useRouter();
+    let screen = APP_NEW_DOMAIN ? router.query.screenChild : router.query.appNameId
+    screen = screen ?? '';
+    if (screen.startsWith(Routes.TEST_SCREEN)) {
+        let offset = screen.lastIndexOf('-');
+        let topicId = -1;
+        if (offset > -1) {
+            offset += 1;
+            topicId = offset > -1 ? parseInt(screen.substring(offset, screen.length)) : -1;
+        }
+        return <TestViewScreen topicId={topicId} appInfoState={appInfoState} />
+    }
+    if (screen.startsWith(Routes.REVIEW_SCREEN)) {
+        return <ReviewViewScreen appInfoState={appInfoState} />
+    }
+    if (screen.length > 0) {
+        let offset = screen.lastIndexOf('-');
+        let topicId = -1;
+        if (offset > -1) {
+            offset += 1;
+            topicId = offset > -1 ? parseInt(screen.substring(offset, screen.length)) : -1;
+        }
+        return <StudyViewScreen appInfoState={appInfoState} topicId={topicId} />
+    }
+    return <ErrorPage />
+}
+
 export async function getServerSideProps(context) {
     const appNameId = APP_NEW_DOMAIN ? APP_NEW_DOMAIN : context.params.appNameId;
     const directoryAppInfos = path.join(process.cwd(), 'src/data/appInfos.json')
@@ -56,48 +87,4 @@ export async function getServerSideProps(context) {
     }
 }
 
-function ScreenChild({ appInfoState }) {
-    if(APP_NEW_DOMAIN){
-        const router = useRouter();
-        let screen = router.query.appNameId
-        screen = screen ?? '';
-        // console.log("screen", screen, 'router.query', router.query)
-        if (screen.startsWith(Routes.TEST_SCREEN)) {
-            let offset = screen.lastIndexOf('-');
-            let topicId = -1;
-            if (offset > -1) {
-                offset += 1;
-                topicId = offset > -1 ? parseInt(screen.substring(offset, screen.length)) : -1;
-            }
-            return <TestViewScreen topicId={topicId} appInfoState={appInfoState} />
-        }
-        if (screen.startsWith(Routes.REVIEW_SCREEN)) {
-            return <ReviewViewScreen appInfoState={appInfoState} />
-        }
-        if (screen.length > 0) {
-            return <StudyViewScreen appInfoState={appInfoState} />
-        }
-        return <h1>Not found page!</h1>
-    } else {
-        const router = useRouter();
-        const { practice, appNameId, screenChild } = router.query
-        let screen = screenChild
-        screen = screen ?? '';
-        if (screen.startsWith(Routes.TEST_SCREEN)) {
-            let offset = screen.lastIndexOf('-');
-            let topicId = -1;
-            if (offset > -1) {
-                offset += 1;
-                topicId = offset > -1 ? parseInt(screen.substring(offset, screen.length)) : -1;
-            }
-            return <TestViewScreen topicId={topicId} appInfoState={appInfoState} />
-        }
-        if (screen.startsWith(Routes.REVIEW_SCREEN)) {
-            return <ReviewViewScreen appInfoState={appInfoState} />
-        }
-        if (screen.length > 0) {
-            return <StudyViewScreen appInfoState={appInfoState} />
-        }
-    }
-}
 export default GameChildScreen
