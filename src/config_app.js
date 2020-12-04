cb90d4a973aa6ca251680e6171a9da18eccb5ea6
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-12-04 09:23:12 +0700

    passemall oke

--- src/config_app.js
+++ src/config_app.js
@@ -1,4 +1,4 @@
-export const APP_NEW_DOMAIN = 5722070642065408;
-export const GA_ID = 'UA-167769768-3';
-export const APP_SHORT_NAME = 'cdl';
+export const APP_NEW_DOMAIN = false;
+export const GA_ID = 'UA-167769768-1';
+export const APP_SHORT_NAME = '';
 export const VERSION = '2.0.5';
