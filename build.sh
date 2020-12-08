9cb067c7b9b4949273e2cd5acb77e2e279dda057
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-12-08 17:33:56 +0700

    passemall version 2.0.10

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 ROOT_PATH="./public/info"
 DATA_PATH="./src/data"
-VERSION="2.0.9"
+VERSION="2.0.10"
 
 downloadFile() {
     curl -o "$DATA_PATH/appInfos.json" "https://micro-enigma-235001.appspot.com/new/api?type=get-map-app"
